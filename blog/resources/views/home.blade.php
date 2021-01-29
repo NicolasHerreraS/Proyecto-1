ac58c2f42824c0db4430fef2b89bf697efcb70a1
NicolasHerreraS <nicolas.herreras@autonoma.edu.co>
2021-01-28 23:50:12 -0500

    Inicio del proyecto
    Proyecto 1 Ingeniería de Software
    Nicolás Herrera Sepúlveda

---
+++ blog/resources/views/home.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <style>
+            body {
+                font-family: 'Nunito';
+            }
+            .titulo{
+                font-size: 30px;
+                float: center;
+                position: fixed;
+                top:0;
+                border-radius: 25px;
+                background: #8D62CF;
+                padding: 10px;
+                margin: 30px;
+                width: 90%;
+                z-index: 100;
+                display: inline-block;
+            }
+            .separator-small{
+                 margin-top:80px;
+            }
+        </style>
+    </head>
+    <body>
+        <div align="center" class="titulo">Bienvenido al inicio de la página</div>
+        <br class="separator-small">
+        <div align="center">
+            <a href="{{ url('/frameworks') }}">Lista de frameworks!</a>
+        </div>
+    </body>
+</html>
